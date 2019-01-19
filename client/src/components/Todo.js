27adcb3c8e39ef47c3afad06fd63d215bc9a6c0d
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-18 22:31:25 -0500

    Add strikethrough when todo is completed.

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleTodo } from '../redux/actions';
+import './Todo.css';
 
-const Todo = ({ todo }) => (
+const cName = completed => (completed ? 'todo-completed' : 'todo-incomplete');
+
+const Todo = ({ toggleTodo, todo }) => (
   <li>
-    <span onClick={() => toggleTodo(todo.id)}>{todo.content}</span>
+    <span
+      className={`todo ${cName(todo.completed)}`}
+      onClick={() => toggleTodo(todo.id)}
+    >
+      {todo.content}
+    </span>
   </li>
 );
 
