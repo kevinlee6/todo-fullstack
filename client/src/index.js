cb33d2cc3bf6fd7272c6775d85b66a7d85b86e6b
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-18 19:25:23 -0500

    Set up redux provider and store.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
