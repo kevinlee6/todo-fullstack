571851e1573a2634be2da71eac4066fa04df9010
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-18 19:30:29 -0500

    Implement redux CRUD actions.

---
+++ client/src/redux/actions.js
@@ -0,0 +1,29 @@
+import { ADD_TODO, DELETE_TODO, EDIT_TODO, TOGGLE_TODO } from './actionTypes';
+
+let nextTodoId = 0;
+
+export const addTodo = content => ({
+  type: ADD_TODO,
+  payload: {
+    id: ++nextTodoId,
+    content,
+  },
+});
+
+export const deleteTodo = id => ({
+  type: DELETE_TODO,
+  payload: { id },
+});
+
+export const editTodo = (id, content) => ({
+  type: EDIT_TODO,
+  payload: {
+    id,
+    content,
+  },
+});
+
+export const toggleTodo = id => ({
+  type: TOGGLE_TODO,
+  payload: { id },
+});
