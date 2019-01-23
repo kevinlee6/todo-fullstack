d280381dc47ba43b363f67f3f2e4dbac164efcc6
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-22 23:51:31 -0500

    Init knexfile and add dotenv.

---
+++ knexfile.js
@@ -0,0 +1,41 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      user: process.env.DB_USER,
+      database: process.env.DB_NAME,
+    },
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password',
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user: 'username',
+  //     password: 'password',
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
+};
