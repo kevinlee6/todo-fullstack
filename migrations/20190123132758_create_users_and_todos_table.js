aa7de9f6da96a6639c63c72c83d106b2c8a73b7e
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-23 13:34:25 -0500

    Combine table creation migrations into one bc of fk constraints.

---
+++ migrations/20190123132758_create_users_and_todos_table.js
@@ -0,0 +1,35 @@
+// need to create users table first then todos table
+// because todos belongs to users
+
+const todos = 'todos';
+const users = 'users';
+const fk = 'user_id'; // foreign key
+const ft = 'users.id'; // foreign table
+
+const usersTable = t => {
+  t.increments();
+  t.string('email').notNullable();
+  t.unique('email');
+  t.string('password').notNullable();
+  t.timestamps();
+};
+
+const todosTable = t => {
+  t.increments();
+  t.integer(fk).unsigned();
+  t.foreign(fk).references(ft);
+  t.index(fk);
+  t.string('content').notNullable();
+  t.boolean('completed')
+    .notNullable()
+    .defaultTo(false);
+  t.timestamps();
+};
+
+exports.up = async db => {
+  await db.schema.createTable(users, usersTable).createTable(todos, todosTable);
+};
+
+exports.down = async db => {
+  await db.schema.dropTableIfExists(todos).dropTableIfExists(users);
+};
