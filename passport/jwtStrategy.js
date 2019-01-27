e2e68a5808135faec8320cc30e39be568d9ee142
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-26 20:54:22 -0500

    Clean up jwt strategy. Fix verify jwt bug bc id was used instead of user_id var.

--- passport/jwtStrategy.js
+++ passport/jwtStrategy.js
@@ -2,15 +2,19 @@ require("dotenv").config;
 const User = require("../models/user");
 const { Strategy, ExtractJwt } = require("passport-jwt");
 
+// 2 week expiration jwt
+const maxAge = 60 * 60 * 24 * 14;
+
 module.exports = new Strategy(
   {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.SECRET
+    secretOrKey: process.env.SECRET,
+    maxAge
   },
   async (payload, cb) => {
     try {
-      const { id } = payload;
-      const user = await User.get({ id: parseInt(id) });
+      const { user_id } = payload;
+      const user = await User.get({ id: parseInt(user_id) });
       return cb(null, user);
     } catch (err) {
       return cb(err);
