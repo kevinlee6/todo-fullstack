f4f641e7b9383f47fc4d9700ae159b0572d10e61
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-09-02 18:30:06 -0400

    Update package-lock.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const cookiesMiddleware = require("universal-cookie-express");
-const cookieParser = require("cookie-parser");
-const passport = require("./passport");
-const users = require("./routes/usersRoutes.js");
-const todos = require("./routes/todosRoutes.js");
-const auth = require("./routes/authRoutes");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const passport = require('./passport');
+const users = require('./routes/usersRoutes.js');
+const todos = require('./routes/todosRoutes.js');
+const auth = require('./routes/authRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,20 +14,19 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookiesMiddleware());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/", auth);
+app.use('/', auth);
 // app.use("/api/users", passport.authenticate("jwt", { session: false }), users);
-app.use("/api/users", users);
-app.use("/api/todos", passport.authenticate("jwt", { session: false }), todos);
+app.use('/api/users', users);
+app.use('/api/todos', passport.authenticate('jwt', { session: false }), todos);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen(PORT, () => {
